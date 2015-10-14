46b8f734b5551d24c8d4b2d59cd6b63fe998956c
Valentyn_Kvasov <Valentyn_Kvasov@epam.com>
2015-10-14 20:31:30 +0300

    SCM

---
+++ gradle/src/main/java/com/gradle/DownloadUtil.java
@@ -0,0 +1,24 @@
+package com.gradle;
+
+import java.awt.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+/**
+ * Created by Valentyn on 10/10/2015.
+ */
+public class DownloadUtil {
+    public static final String filePath = System.getenv("TEMP") + "/NewTxt.txt";
+
+    public static void main(String[] args) throws Exception{
+        URL website = new URL("https://dl.dropboxusercontent.com/u/98396761/NewTxt.txt");
+        try(ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+            FileOutputStream fos = new FileOutputStream(filePath)) {
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        }
+        Desktop.getDesktop().open(new File(filePath));
+    }
+}
